aeea2fbc93976306b67afcd2dd48348183d80e61
Mateusz Tadla <mat@scalepoint.com>
2023-11-04 16:09:40 +0100

    update

--- src/Ch2_ControlInstructions/Ex1_IfDemo.java
+++ src/Ch2_ControlInstructions/Ex1_IfDemo.java
@@ -1,7 +1,23 @@
 package Ch2_ControlInstructions;
 
+import java.util.Scanner;
+
 public class Ex1_IfDemo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("podaj liczbe");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("liczba jest parzysta");
+        } else {
+            System.out.println("liczba nie jest parzysta");
+        }
+
+        System.out.println("koniec programu");
+
+
 
     }
 }
---
+++ src/Ch2_ControlInstructions/Ex2_If.java
@@ -0,0 +1,14 @@
+package Ch2_ControlInstructions;
+
+public class Ex2_If {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+Pobierz z konsoli dwie liczby całkowite, następnie porównaj je i wyświetl stosowny komunikat z wynikiem.
+	„a jest mniejsze od b”
+ 	„a jest większe od b”
+	„a jest równe b”
+ */
