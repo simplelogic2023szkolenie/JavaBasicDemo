6b1bb25ae3d29b46182334945c33ca5a5ce19688
Mateusz Tadla <mat@scalepoint.com>
2023-11-04 15:20:10 +0100

    update

---
+++ src/Ch1_VariablesDataTypes/Ex7_MOexercise.java
@@ -0,0 +1,15 @@
+package Ch1_VariablesDataTypes;
+
+public class Ex7_MOexercise {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+1. Zadeklaruj dwie zmienne typu `int` i przypisz im dowolne wartości.
+2. Dla każdej z operacji: dodawania, odejmowania, mnożenia i dzielenia, zadeklaruj osobną zmienną, w której zapiszesz wynik danej operacji.
+3. Wykorzystując operatory matematyczne (`+`, `-`, `*`, `/`), oblicz wyniki dla zadeklarowanych wcześniej zmiennych.
+4. Wydrukuj wyniki operacji w następującym formacie: "Wynik [nazwa operacji]: [wynik]".
+
+ */
