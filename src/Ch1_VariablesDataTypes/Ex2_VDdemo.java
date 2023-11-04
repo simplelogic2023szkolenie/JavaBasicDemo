0e86e81a6bb8cfb811872a8c2d7004ccbfeb6f99
Mateusz Tadla <mat@scalepoint.com>
2023-11-04 14:26:50 +0100

    new ex

---
+++ src/Ch1_VariablesDataTypes/Ex2_VDdemo.java
@@ -0,0 +1,28 @@
+package Ch1_VariablesDataTypes;
+
+public class Ex2_VDdemo {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+Opis zadania:
+1. Zadeklaruj zmienne następujących typów:
+    - int
+    - double
+    - char
+    - boolean
+    - String
+
+2. Przypisz im odpowiednie wartości. Na przykład:
+    - int - dowolna liczba całkowita
+    - double - dowolna liczba zmiennoprzecinkowa
+    - char - dowolny znak
+    - boolean - wartość true lub false
+    - String - dowolny ciąg znaków
+
+3. Użyj funkcji System.out.println do wydrukowania wartości każdej z zadeklarowanych zmiennych na ekran.
+
+
+     */
+}
