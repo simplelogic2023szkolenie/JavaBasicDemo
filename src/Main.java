7fc6e0cd198049055dbd747bd59cf8fc4fa7663e
Mateusz Tadla <mat@scalepoint.com>
2023-11-04 14:19:59 +0100

    main update

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello 1");
+        System.out.println("Hello 1");
         int a = 10;
         System.out.println(a);
 
