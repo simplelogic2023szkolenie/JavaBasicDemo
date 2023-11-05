0170e24150da03da04dc14c2a6ab40b0739dad6d
Mateusz Tadla <mat@scalepoint.com>
2023-11-05 12:57:22 +0100

    update

--- src/Ch4_Loops/Ex11_AllLoops.java
+++ src/Ch4_Loops/Ex11_AllLoops.java
@@ -1,8 +1,35 @@
 package Ch4_Loops;
 
+import java.util.Scanner;
+
 public class Ex11_AllLoops {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number");
+        int number = scanner.nextInt();
+
+        //for
+        for (int i = number; i > 0; i--) {
+            System.out.println("seconds to start: " + i);
+        }
+        System.out.println("start!\n");
+
+        //while
+        int whileCounter = number;
+        while (whileCounter > 0) {
+            System.out.println("seconds to start: " + whileCounter);
+            whileCounter--;
+        }
+        System.out.println("start!\n");
+
+        //do while
+        int doWhileCounter = number;
+        do {
+            System.out.println("seconds to start: " + doWhileCounter);
+            doWhileCounter--;
+        } while (doWhileCounter > 0);
+        System.out.println("start!");
     }
 }
 
