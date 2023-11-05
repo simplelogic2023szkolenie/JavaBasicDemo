0d1f3b3307dc924739a066139c7f48d6d5410c4f
Mateusz Tadla <mat@scalepoint.com>
2023-11-05 11:36:59 +0100

    update

---
+++ src/Ch4_Loops/Ex0_PlusVsConcat.java
@@ -0,0 +1,18 @@
+package Ch4_Loops;
+
+public class Ex0_PlusVsConcat {
+    public static void main(String[] args) {
+        String text = "World";
+        int number = 10;
+        System.out.println(text+number);
+
+        // poniższe zakomentowane ponieważ byłby błąd kompilacji
+//        System.out.println(text.concat(number));
+
+        String s1 = null;
+        String s2 = "World";
+        System.out.println(s1 + s2);
+        // poniższy kod wyrzuca wyjątek:
+        System.out.println(s1.concat(s2));
+    }
+}
