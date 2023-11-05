0d4ff85a763ec7bc20314c6b81a75bcadecd0432
Mateusz Tadla <mat@scalepoint.com>
2023-11-05 14:16:57 +0100

    update

---
+++ src/Ch5_Arrays/Ex5_Arrays.java
@@ -0,0 +1,20 @@
+package Ch5_Arrays;
+
+public class Ex5_Arrays {
+    public static void main(String[] args) {
+        int n = 10;
+        int[] tab = new int[n];
+    }
+}
+
+/*
+Stwórz zmienną int n i przypisz to niej dowolną wartość > 0.
+Stwórz tablice n elementową.
+Przy pomocy pętli ustaw wartości przechowywane w tablicy. Do każdego elementu tablicy przypisz kwadrat jego indeksu.
+Czyli np. tab[3] powinno przechowywać liczbę 9
+Na koniec wydrukuj liczby z tabeli używając formatowania:
+		element o indeksie 1 to 1
+		element o indeksie 2 to 4
+
+
+ */
