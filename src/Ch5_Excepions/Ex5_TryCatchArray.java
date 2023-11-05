11b8fe3b7a508df0de3fb3fe5197e3fcaaac119c
Mateusz Tadla <mat@scalepoint.com>
2023-11-05 16:52:09 +0100

    update

--- src/Ch5_Excepions/Ex5_TryCatchArray.java
+++ src/Ch5_Excepions/Ex5_TryCatchArray.java
@@ -2,7 +2,19 @@ package Ch5_Excepions;
 
 public class Ex5_TryCatchArray {
     public static void main(String[] args) {
+        int[] tab = new int[5];
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println("wartosc i: " + i);
+            try {
+                tab[i] = i;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Unfortunately you are out of range of the array");
+                System.out.println("wychodze z petli");
+                break;
+            }
+        }
+        System.out.println("THE END");
     }
 }
 
