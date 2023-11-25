b7254e7f7700ac4865d25156bf15aa5b34c1be09
Mateusz Tadla <mat@scalepoint.com>
2023-11-25 11:58:13 +0100

    update

--- src/Ch6_Methods/Ex11_SimpleCalculator.java
+++ src/Ch6_Methods/Ex11_SimpleCalculator.java
@@ -2,7 +2,26 @@ package Ch6_Methods;
 
 public class Ex11_SimpleCalculator {
     public static void main(String[] args) {
+        System.out.println(add(2,5));
+        System.out.println(subtract(10,2));
+        System.out.println(divide(10,2));
+        System.out.println(multiply(2,5));
+    }
 
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
     }
 }
 /*
