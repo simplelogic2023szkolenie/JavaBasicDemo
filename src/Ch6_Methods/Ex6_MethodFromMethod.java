ef1f740f03340f9a96083bc8bedf70a684ba89b8
Mateusz Tadla <mat@scalepoint.com>
2023-11-25 10:50:48 +0100

    update

---
+++ src/Ch6_Methods/Ex6_MethodFromMethod.java
@@ -0,0 +1,22 @@
+package Ch6_Methods;
+
+public class Ex6_MethodFromMethod {
+    public static void main(String[] args) {
+        int x = getNumber();
+        System.out.println(x);
+        SayHello();
+    }
+    public static void SayHello() {
+        System.out.println("Hello!!!!!!");
+        Goodbye();
+        System.out.println("end of say hello method");
+    }
+    public static int getNumber() {
+        System.out.println("Im inside getNumber");
+        SayHello();
+        return 15;
+    }
+    public static void Goodbye() {
+        System.out.println("Goodbye!!");
+    }
+}
---
+++ src/Ch6_Methods/Ex7_MethodsWithScannerPractice.java
@@ -0,0 +1,15 @@
+package Ch6_Methods;
+
+public class Ex7_MethodsWithScannerPractice {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+// zaimplementować 2 metody
+// pierwsza ma zwracac: int (pobrany od użytkownika)
+// druga: String (pobrany od użytkownika)
+//
+// w metodzie main() uruchamiany wszystkie metody
+// i drukujemy do kosoli wartości jakie zostały zwrocone
