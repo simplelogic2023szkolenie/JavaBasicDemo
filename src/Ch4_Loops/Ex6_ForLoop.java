daeb5d44fb031227172e842ae56769e7229e4f63
Mateusz Tadla <mat@scalepoint.com>
2023-11-25 11:08:50 +0100

    update

--- src/Ch4_Loops/Ex6_ForLoop.java
+++ src/Ch4_Loops/Ex6_ForLoop.java
@@ -3,6 +3,7 @@ package Ch4_Loops;
 public class Ex6_ForLoop {
     public static void main(String[] args) {
         // rozwiazanie Zadanie 2
+
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 4 == 0) {
--- src/Ch6_Methods/Ex7_MethodsWithScannerPractice.java
+++ src/Ch6_Methods/Ex7_MethodsWithScannerPractice.java
@@ -1,15 +1,43 @@
 package Ch6_Methods;
 
+import java.util.Scanner;
+
 public class Ex7_MethodsWithScannerPractice {
     public static void main(String[] args) {
-
+        int number = getNumberFromUser();
+        System.out.println(number);
+        System.out.println(getNumberTextUser());
     }
 
+    public static int getNumberFromUser(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj liczbę");
+        // najpierw przypisuje wartosć do zmiennej,
+        int number = scanner.nextInt();
+        // a potem zwacamy wartość zmiennej
+        return number;
+    }
+
+    public static String getNumberTextUser(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj tekst");
+        // tutaj od razu zwracamy to co wpisał user
+        // bez wcześniejszego zapisywania do zmiennej
+        return scanner.nextLine();
+    }
 }
 
-// zaimplementować 2 metody
-// pierwsza ma zwracac: int (pobrany od użytkownika)
-// druga: String (pobrany od użytkownika)
-//
-// w metodzie main() uruchamiany wszystkie metody
-// i drukujemy do kosoli wartości jakie zostały zwrocone
+// zrób metode void printGetNumberMsg() -> ma drukować tekst "podaj liczbe"
+// zrób metode void printGetTextMsg() -> ma drukować tekst "podaj tekst"
+
+// następnie zrób 2 metody
+// >PIERWSZA ma zwracac: int (pobrany od użytkownika),
+// >>>zanim go pobierze powinna uruchomić metode printGetNumberMsg()
+// >>>aby wydrukować użytkonikowi odpowiednią prośbę
+
+// >DRUGA ma zwracac: String (pobrany od użytkownika)
+// >>>zanim go pobierze powinna uruchomić metode printGetTextMsg()
+// >>>aby wydrukować użytkonikowi odpowiednią prośbę
+
+// w metodzie main() uruchom metody, które zwracają int oraz string
+// wydrukuj do kosoli wartości jakie zostały zwrocone
