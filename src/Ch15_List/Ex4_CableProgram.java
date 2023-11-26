504a3399ea4d54a47dd400e5c167e206ec2f7308
Mateusz Tadla <mat@scalepoint.com>
2023-11-26 13:56:49 +0100

    update

---
+++ src/Ch15_List/Ex4_CableProgram.java
@@ -0,0 +1,19 @@
+package Ch15_List;
+
+public class Ex4_CableProgram {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ *
+ * Stwórz klasę o nazwie Cable (wraz z konstruktorem) zawierającą:
+ * Pole: String type
+ * Metodę: public void print() drukującą tekst: "Jest to kabel typu xxx"
+ *
+ * W klasie CableProgram z metodą main() stwórz listę:List<Cable> cables = new ArrayList<>();
+ * Stwórz 5 obiektów klasy Cable, a następnie dodaj je do listy używając metody cables.add(xxxx)
+ * Na koniec przy pomocy pętli for-each przeiteruj po liście cables i dla każdego z elementów uruchom metodę print().
+ * Na koniec programu wydrukuj do konsoli ile elementów zawiera lista
+ */
