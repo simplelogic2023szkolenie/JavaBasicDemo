f72989af21bb4737786e756ce19a2782ebfb641a
Mateusz Tadla <mat@scalepoint.com>
2023-11-26 11:31:19 +0100

    update

--- src/Ch14_ObjectAsField/Ex2_Program.java
+++ src/Ch14_ObjectAsField/Ex2_Program.java
@@ -7,27 +7,37 @@ public class Ex2_Program {
 }
 
 
-/*
-stworz klase Engine:
- - pole int horsePower
- - pole int maxSpeed
- - konstruktor
-  - metode String getEngineData() -> na zwacac horsePower maxSpeed
-
-
- stworz klase Vehicle:
- - pole String brand
- - pole int yearOfProduction
- - pole Engine engine
-  - konstruktor
-  - metode String getVehicleData() -> ma zwacac brand yearOfProduction engine.getEngineData()
-
- stwórz klase Program z metoda main()
- - stwórz 2 obiekty klasy Engine
- - stwórz 3 obiekty klasy Vehicle -> użyj obiektów klasy Enine do konstroka Engine
-
- na obiektach klasy Vehicle uruchom System.out.prinln(xxxxx.getVehicleData())
-
-
-
+/**
+ *
+ * 1. Klasa Engine (Silnik):
+ *    - Atrybuty:
+ *      - int horsePower - moc w koniach mechanicznych.
+ *      - int maxSpeed - maksymalna prędkość.
+ *    - Konstruktor: Tworzy instancję klasy Engine z odpowiednimi parametrami.
+ *    - Metoda: String getEngineData()
+ *      - Zadanie: Zwraca dane silnika w formacie: moc (horsePower) i maksymalna prędkość (maxSpeed).
+ *
+ * 2. Klasa Vehicle (Pojazd):
+ *    - Atrybuty:
+ *      - String brand - marka pojazdu.
+ *      - int yearOfProduction - rok produkcji.
+ *      - Engine engine - obiekt klasy Engine reprezentujący silnik pojazdu.
+ *    - Konstruktor: Tworzy instancję klasy Vehicle z odpowiednimi parametrami.
+ *    - Metoda: String getVehicleData()
+ *      - Zadanie: Zwraca dane pojazdu w formacie: marka (brand), rok produkcji (yearOfProduction) oraz dane silnika uzyskane za pomocą engine.getEngineData().
+ *
+ * 3. Klasa Program z metodą main():
+ *    - Zadanie:
+ *      - Stwórz 2 obiekty klasy Engine:
+ *        - Engine dieselEngine = new Engine(.......); // Silnik Diesla o mocy 130 KM i maks. prędkości 200 km/h
+ *        - Engine petrolEngine = new Engine(......); // Silnik benzynowy o mocy 180 KM i maks. prędkości 240 km/h
+ *      - Stwórz 3 obiekty klasy Vehicle:
+ *        - Vehicle toyota = new Vehicle(.....); // Toyota z 2020 roku z silnikiem Diesla
+ *        - Vehicle honda = new Vehicle(.....); // Honda z 2018 roku z silnikiem benzynowym
+ *        - Vehicle bmw = new Vehicle(.....); // BMW z 2021 roku z silnikiem Diesla
+ *      - Wyświetl informacje o pojazdach:
+ *        - System.out.println(toyota.getVehicleData());
+ *        - System.out.println(honda.getVehicleData());
+ *        - System.out.println(bmw.getVehicleData());
+ *
  */
