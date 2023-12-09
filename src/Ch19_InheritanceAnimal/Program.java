8403be450bb9656a1f17442f196aa0c8b169fb2e
Mateusz Tadla <mtadla@sii.pl>
2023-12-09 14:57:07 +0100

    update

--- src/Ch19_InheritanceAnimal/Program.java
+++ src/Ch19_InheritanceAnimal/Program.java
@@ -7,7 +7,7 @@ public class Program {
 }
 
 
-/*
+/**
 Utwórz klasę Animal (wraz z konstruktorem) zawierającą:
 pole: private String name
 metodę public void eat() wypisującą text "I can eat"
@@ -17,4 +17,14 @@ Utwórz klasę Dog (wraz z konstruktorem – super() + inicjalizacja pola height
 Pole: private int height
 Metodę: void bark() wypisującą text "woof woof!" oraz metodę introduce() wypisującą text: "I'm dog, my  name is xxx and I have xxx height"
 
- */
+
+
+Utwórz klasę Bird (wraz z konstruktorem – super() + inicjalizacja pól wingspan i canFly) dziedziczącą po Animal zawierającą:
+pola:
+       private int wingspan, private boolean canFly
+metody:
+public void introduce() wypisującą text: "I'm bird, my name is xxx and I have xxx wingspan"
+public void fly() wypisującą text "I can fly!" dla canFly==true, a w przeciwnym razie "I can't fly"
+
+W klasie Zoo z metodą main() stwórz  2 obiekty klasy Bird i uruchom na nich metody eat(), introduce(), oraz fly() oraz 2 obiekty klasy Dog i uruchom dla nich metody eat(), introduce() oraz bark()
+ **/
