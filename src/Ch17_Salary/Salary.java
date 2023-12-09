99ca37eec1db0ab587a29251145742651cfcb2c2
Mateusz Tadla <mtadla@sii.pl>
2023-12-09 10:19:30 +0100

    update

--- src/Ch17_Salary/Salary.java
+++ src/Ch17_Salary/Salary.java
@@ -6,6 +6,8 @@ public class Salary {
 
 /**
  * Program "Lista płac:"
+
+ cz1
  * Stwórz klasę Employee przechowującą imię, nazwisko oraz kwotę wypłaty pracownika.
  * dodaj odpowiedni konstruktor oraz gettery/settery.
  * Stwórz również metodę getAllData(), która zwróci tekst w formacie „Salary for IMIE NAZWISKO is WYPLATA”
@@ -13,5 +15,16 @@ public class Salary {
 
  * Stwórz klasę Company przechowującą listę pracowników (ArrayList<Employee>)
 
-
+ cz2
+ * Stwórz klasę InputHelper posiadającą metody:
+ *
+ * - public String getTextFromUser(String msg)
+ *     powinna wydrukować do konsoli text parametru msg
+ *     pobrać od użytkownika tekst używając Scannera
+ *     zwrócić tekst pobrany od użytkownika
+ *
+ * - public String getNumberFromUser(String msg)
+ *     powinna wydrukować do konsoli text parametru msg
+ *     pobrać od użytkownika int używając Scannera
+ *     zwrócić liczbę pobraną od użytkownika
  */
