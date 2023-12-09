a66b9a680ddbebdbf4a04d6e6e8ab1a2f5da08de
Mateusz Tadla <mtadla@sii.pl>
2023-12-09 11:12:32 +0100

    update

--- src/Ch17_Salary/Salary.java
+++ src/Ch17_Salary/Salary.java
@@ -27,4 +27,26 @@ public class Salary {
  *     powinna wydrukować do konsoli text parametru msg
  *     pobrać od użytkownika int używając Scannera
  *     zwrócić liczbę pobraną od użytkownika
+ *
+ cz3
+ * W klasie SalaryProgram i metodzie main()
+ *
+ * stwórz zmienną employees typu ArrayList<Employee> i zainicjalizuj pustą listę
+ *
+ * w pętli for (5 powtórzeń) za każdym razem wykonaj:
+ *      pobierz od użytkonika - imie, nazwisko, wyplate (używając metod z InputHelper)
+ *      uzyj tych wartości to stworzenia obietku klasy Employee
+ *      dodaj stworzonego employee do listy employees
+ *
+
+ cz4
+ * w klasie Company stwórz konstruktor, który zainicjalizuje listę przechowywanych pracowników
+ * stwórz metoę displaySalarySum() która wyświetli sumę zarobków wszystkich pracowników
+ *
+ *
+ * w klasie SalaryProgram i metodzie main() stwórz obiekt klasy company i przekaż
+ * do konstruktora utworzoną wcześniej listę pracowników
+ * no obiektice company uruchom metodę displaySalarySum() i sprawdz czy wydrukowała poprawną wartość
+
+ *
  */
