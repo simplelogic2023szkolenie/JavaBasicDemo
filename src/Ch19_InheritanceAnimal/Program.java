6a68561b7e2ba636c95b188ded60e8af3c0693cb
Mateusz Tadla <mtadla@sii.pl>
2023-12-09 14:44:18 +0100

    update

---
+++ src/Ch19_InheritanceAnimal/Program.java
@@ -0,0 +1,20 @@
+package Ch19_InheritanceAnimal;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*
+Utwórz klasę Animal (wraz z konstruktorem) zawierającą:
+pole: private String name
+metodę public void eat() wypisującą text "I can eat"
+oraz getter do pola name
+
+Utwórz klasę Dog (wraz z konstruktorem – super() + inicjalizacja pola height) dziedziczącą po Animal zawierającą:
+Pole: private int height
+Metodę: void bark() wypisującą text "woof woof!" oraz metodę introduce() wypisującą text: "I'm dog, my  name is xxx and I have xxx height"
+
+ */
