b92fa3eaa86ba8f697eb5ebb21c2aae4975b27e8
Mateusz Tadla <mat@scalepoint.com>
2024-01-28 09:38:51 +0100

    update

--- src/Ch26_MovieLibrary/LibraryMain.java
+++ src/Ch26_MovieLibrary/LibraryMain.java
@@ -6,3 +6,25 @@ public class LibraryMain {
 
     }
 }
+
+
+/**
+ *
+ * Stwórz program o nazwie MovieLibrary. Powinien on przechowywać listę filmów.
+ *
+ * Każdy film powinien mieć: tytuł, rok wydania, reżysera oraz listę aktorów
+ *
+ * Reżyser oraz aktorzy powinni mieć oddzielnie imię i nazwisko.
+ *
+ * Zastosuj tutaj programowanie obiektowe – stwórz klasy MovieLibrary, Movie, Director, Actor wraz z potrzebnymi polami klasy i metodami.
+ *
+ * W metodzie main() stwórz obiekty 5 filmów - każdy z przynajmniej 3 aktorami (część aktorów powinna występować w więcej niż filmie) – i dodaj je do listy.
+ *
+ * Stwórz menu w programie, które będzie wyświetlane użytkownikowi w konsoli, wraz z obsługą wybranych przez niego opcji:
+ *
+ * 	1. Podaj dwie daty aby wyświetlić nazwy filmów nagranych pomiędzy tymi datami
+ * 	2. Wyświetl wszystkie informacje o losowym filmie
+ * 	3. Podaj imię i nazwisko aktora aby wyświetlić nazwy filmów w jakich grał
+ *
+ *
+ */
