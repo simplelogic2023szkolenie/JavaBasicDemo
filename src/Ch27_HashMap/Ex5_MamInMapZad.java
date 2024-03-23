8f020f046116af235ef12f88269549bf54a67875
Mateusz Tadla <mtadla@dtiq.com>
2024-03-23 11:16:51 +0100

    update

--- src/Ch27_HashMap/Ex5_MamInMapZad.java
+++ src/Ch27_HashMap/Ex5_MamInMapZad.java
@@ -1,8 +1,30 @@
 package Ch27_HashMap;
 
+import java.util.HashMap;
+
 public class Ex5_MamInMapZad {
     public static void main(String[] args) {
+        HashMap<String, Integer> clothes = new HashMap<>();
+        HashMap<String, Integer> agd = new HashMap<>();
+        HashMap<String, Integer> rtv = new HashMap<>();
 
+        clothes.put("trousers", 100);
+        clothes.put("thirts", 123);
+        clothes.put("jackets", 12);
+
+        agd.put("washingmachines", 23);
+        agd.put("fridges", 34);
+
+        rtv.put("tvs", 52);
+        rtv.put("soundbars", 67);
+
+        HashMap<Integer,  HashMap<String, Integer>> categories = new HashMap<>();
+
+        categories.put(11,clothes);
+        categories.put(12,agd);
+        categories.put(13,rtv);
+
+        System.out.println(categories);
     }
 }
 
