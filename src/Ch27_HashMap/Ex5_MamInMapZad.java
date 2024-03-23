d1f9ca0d4ff0f6dba1f805a9e5f13e1f12d194c0
Mateusz Tadla <mtadla@dtiq.com>
2024-03-23 11:02:45 +0100

    update

---
+++ src/Ch27_HashMap/Ex5_MamInMapZad.java
@@ -0,0 +1,16 @@
+package Ch27_HashMap;
+
+public class Ex5_MamInMapZad {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ *
+ * zorb 3x mapy <String, Integer> klucz - nazwa kategorii w sklepie, oraz wartość - ilość produktow
+ *
+ * zrob 1x mape zawieracjaca mape jako wartosc  <Integer, <String, Integer>>  klucz - ID kategorii, wartość to mapa z pkt 1
+ *
+ * dodaj do tej mapy, 3 mapy z pkt 1
+ */
